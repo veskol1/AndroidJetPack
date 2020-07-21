bbc042d7924064d07d8bb99eb16b9eb6d76cd84f
97254 <veskol1@gmail.com>
2020-07-22 00:58:21 +0300

    add buttons navigation functionality

--- app/src/main/java/com/example/jetpack/view/DetailFrgament.kt
+++ app/src/main/java/com/example/jetpack/view/DetailFrgament.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.jetpack.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFrgament : Fragment() {
 
@@ -18,4 +20,12 @@ class DetailFrgament : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(DetailFrgamentDirections.actionDetailFrgamentToListFragment())
+        }
+    }
 }
--- app/src/main/java/com/example/jetpack/view/ListFragment.kt
+++ app/src/main/java/com/example/jetpack/view/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.jetpack.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -18,4 +20,11 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(ListFragmentDirections.actionListFragmentToDetailFrgament())
+        }
+    }
 }
