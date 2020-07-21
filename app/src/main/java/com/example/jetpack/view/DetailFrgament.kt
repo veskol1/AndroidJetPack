540a7998098dbb38b1c1a853ed80a13b4553c8b4
97254 <veskol1@gmail.com>
2020-07-22 00:30:27 +0300

    add navigation and list fragment + detail fragment

---
+++ app/src/main/java/com/example/jetpack/view/DetailFrgament.kt
@@ -0,0 +1,21 @@
+package com.example.jetpack.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jetpack.R
+
+class DetailFrgament : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_frgament, container, false)
+    }
+
+}
