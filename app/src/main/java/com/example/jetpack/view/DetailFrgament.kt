aeab0d3c09e233f831832c7d2b23a2fe5b5e9cad
97254 <veskol1@gmail.com>
2020-07-22 01:04:10 +0300

    test coomit

--- app/src/main/java/com/example/jetpack/view/DetailFrgament.kt
+++ app/src/main/java/com/example/jetpack/view/DetailFrgament.kt
@@ -16,7 +16,7 @@ class DetailFrgament : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
